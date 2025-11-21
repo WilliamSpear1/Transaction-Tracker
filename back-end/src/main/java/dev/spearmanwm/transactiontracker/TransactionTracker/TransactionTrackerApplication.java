d135895aab13a519bf2ffdf79d8a1c808dc5cb8f
William Spearman <spearmanwm@gmail.com>
2025-11-21 11:02:01 -0500

    Inital - commit for backend.

---
+++ back-end/src/main/java/dev/spearmanwm/transactiontracker/TransactionTracker/TransactionTrackerApplication.java
@@ -0,0 +1,13 @@
+package dev.spearmanwm.transactiontracker.TransactionTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionTrackerApplication.class, args);
+	}
+
+}
